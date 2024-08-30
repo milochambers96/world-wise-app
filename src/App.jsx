a3641460b0379b64fe8d91a0b741351002857786
Milo Chambers <chambersmilo@gmail.com>
2024-08-30 15:40:48 +0100

    Fetched countries data with rest API and implemented a basic rendering of the countries via a DisplayCountries Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import DisplayCountries from './Components/DisplayCountries';
 
 const App = () => {
 
+  const [countries, setCountries] = React.useState([]);
+  const [search, setSearch] = React.useState('');
+
+  async function fetchCountries() {
+    const resp = await fetch("https://restcountries.com/v3.1/all");
+    const countryData = await resp.json();
+    setCountries(countryData);
+  }
+
+  React.useEffect(() => {
+    fetchCountries();
+  }, [])
+  // console.log(countries)
+
+  function handleSearch(e) {
+    setSearch(e.target.value)
+  }
+
+  const filteredCountries = countries && countries.filter((country) => {
+    return country.name.official.toLowerCase().includes(search.toLowerCase()) || 
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
-    <h1>World Wise</h1>
+    <div>
+      <h1>World Wise</h1>
+      <h4>Your Passport to Global Knowledge.</h4>
+      <input id="search-bar" type="search" placeholder="Search by country..." name="search" value={search} onChange={handleSearch}>
+      </input>
+      <br />
+      <br />
+      <div id="countries-container">
+        {countries ? countries.map((country, index) => {
+          return <DisplayCountries 
+          key = {country.cca3} 
+          name = {country.name.common}
+          flag = {country.flags.png}
+          capital = {country.capital}
+          />
+         }) : <p>Spinning the globe 🌍</p>}
+      
+      </div>
+    </div>
   );
 }
 
